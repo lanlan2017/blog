c55d84f48664a0b5a60aa8cedf4ea995cef0125e
lanlan2017 <18251956727@163.com>
2019-11-05 11:19:40 +0800

    重名名OpenGitBash.bat为HexoD.bat,生成网站目录,修改.gitignore

--- HexoD.bat
+++ HexoD.bat
@@ -3,6 +3,8 @@ echo 切换盘符 %~d0
 %~d0
 echo 进入当前目录 %~dp0
 cd %~dp0
+echo 生成自定义目录
+java -jar "E:\dev2\idea_workspace\HexoTools\runable\FM.jar" toc %~dp0source\_posts\
 echo 打开 git-bash
 start git-bash --cd=%~dp0
 REM pause
