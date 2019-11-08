587f99ac2a2708f133c6a2cc78d4db62fa8a720a
lanlan2017 <18251956727@163.com>
2019-11-08 19:31:24 +0800

    修改HexoD.bat,发布博客的时候删除_post目录下的html文件

--- HexoD.bat
+++ HexoD.bat
@@ -1,8 +1,17 @@
 @echo off
 echo 切换盘符 %~d0
 %~d0
-echo 进入当前目录 %~dp0
-cd %~dp0
+echo 进入当前目录 %~dp0source\_posts\
+cd %~dp0source\_posts\
+echo.
+echo ############################################################### 删除所有的pcm文件
+echo.
+for /r . %%i in (*.html) do (
+	echo     删除:"%%i"
+	del "%%i"
+)
+echo.
+echo ############################################################### 删除所有的xmp文件
 echo 生成自定义目录
 java -jar "E:\dev2\idea_workspace\HexoTools\runable\FM.jar" toc %~dp0source\_posts\
 echo 打开 git-bash
