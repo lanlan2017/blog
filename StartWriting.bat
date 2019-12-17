2e6003f0c9d1703d1b290baefdd3aaffaa371d53
lanlan2017 <18251956727@163.com>
2019-12-18 00:09:03 +0800

    更新批处理文件

--- StartWriting.bat
+++ StartWriting.bat
@@ -6,11 +6,12 @@ REM 进入当前目录
 echo %~dp0
 set blogRoot=%~dp0
 cd %blogRoot%
-REM 拉取远程分支
-git pull origin master
-REM 在文章根目录下打开文件资源管理器
+echo 打开post目录:%~dp0source\_posts
 set postRoot=%~dp0source\_posts
 explorer %postRoot%
-REM 站点目录下打开git-bash
+echo 在%blogRoot%目录下打开git-bash
 start git-bash --cd=%blogRoot%
 REM pause
+echo 拉取远程分支
+git pull origin master
+REM 在文章根目录下打开文件资源管理器
