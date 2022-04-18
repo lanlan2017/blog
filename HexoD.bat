c99c4165b76f91b0dfae1dc1dc0fd2f26850e7db
lanlan2017 <18251956727@163.com>
2022-04-18 21:29:39 +0800

    修改HexoD.bat

--- HexoD.bat
+++ HexoD.bat
@@ -12,7 +12,7 @@ for /r . %%i in (*.html) do (
 )
 echo.
 echo ############################################################### 删除所有的html文件 结束
-echo 生成自定义目录
+echo 生成自定义dir/页面
 java -jar "G:\dev2\idea_workspace\MyJavaTools\runable\Commands.jar" hexo toc %~dp0source\_posts\
 
 echo 在站点目录:%~dp0 打开git-bash
